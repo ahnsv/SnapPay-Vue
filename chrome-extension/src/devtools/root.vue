e6b24a784772641bc52d42126cb3c39020a85646
ahnsv <ahnsv@bc.edu>
2018-10-23 23:00:56 +0900

    chrome extension branch push

---
+++ chrome-extension/src/devtools/root.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+    div devtools
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  
+})
+</script>
+
+<style lang="scss">
+  div {
+    color: blue
+  }
+</style>
---
+++ chrome-extension/src/options/root.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+    div options
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  
+})
+</script>
+
+<style>
+  div {
+    color: blue
+  }
+</style>
