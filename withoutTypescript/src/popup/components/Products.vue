d6220360a7d23fe1885f1539358aa6fdf55cb769
ahnsv <ahnsv@bc.edu>
2018-10-24 10:17:09 +0900

    ui add

---
+++ withoutTypescript/src/popup/components/Products.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-layout row wrap>
+        ProductData
+    </v-layout>
+</template>
+<script>
+import Product from "./Product";
+import '@mdi/font/css/materialdesignicons.css'
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+export default {
+name: 'Products',
+data: () => {
+    return {
+        productsData: [
+        {
+            'title': 'iPhone XS',
+            'subtitle': 'Pretty af',
+            'imgSrc': '../../../static/images/iphone-xs.jpg'
+        },
+        {
+            'title': 'Galaxy Note 9',
+            'subtitle': 'Pretty af',
+            'imgSrc': '../../../static/images/galaxy-note-9.jpg'
+        },
+        {
+            'title': 'Pixel 3',
+            'subtitle': 'Pretty af',
+            'imgSrc': '../../../static/images/pixel_3.jpg'
+        }
+    ]
+    }
+},
+components: {
+    Product
+},
+
+}
+</script>
+
