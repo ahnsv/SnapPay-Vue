e99206cb59568ef51d2a1a7f9606fea131700c68
Humphrey Ahn <ahnsv@bc.edu>
2018-10-24 23:45:57 +0900

    thinking of app dataflow

--- withoutTypescript/src/backend/index.js
+++ withoutTypescript/src/backend/index.js
@@ -1,1 +1,9 @@
 console.log('background !')
+
+// transaction data
+// checkout -> localstorage (storage.js>set) POST
+// profile <- localstorage (storage.js>get) GET
+
+// api server
+// localstorage -> db
+// localstorage -> server
