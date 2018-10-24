992313561f0fca07f60ff9e7aae2184c62cd174b
ahnsv <ahnsv@bc.edu>
2018-10-24 10:17:09 +0900

    ui add

---
+++ withoutTypescript/src/popup/components/Checkout.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-layout row wrap>
+        <v-flex xs12>
+            <v-card>
+                <v-card-title primary-title>
+                    Checkout
+                </v-card-title>
+            </v-card>
+        </v-flex>
+    </v-layout>
+</template>
+<script>
+import '@mdi/font/css/materialdesignicons.css'
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+export default {
+    name: 'Checkout'
+}
+</script>
+
