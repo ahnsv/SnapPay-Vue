2a9a6a00c421f3760db6b77f8d9424ccef0b6529
ahnsv <ahnsv@bc.edu>
2018-10-24 23:17:48 +0900

    vue ui changed, webpack build to chrome extension works

--- withoutTypescript/src/popup/components/Product.vue
+++ withoutTypescript/src/popup/components/Product.vue
@@ -1,50 +1,40 @@
 <template>
-    <v-layout row>
+  <v-layout row>
     <v-flex xs12 sm6 offset-sm3>
       <v-card>
-        <v-img
-          src=""
-          height="400px"
-        >
-        </v-img>
-
-        <v-card-title primary-title>
-          <div>
-            <div class="headline"></div>
-            <span class="grey--text"></span>
-          </div>
-        </v-card-title>
-
-        <v-card-actions>
-          <v-btn flat>Share</v-btn>
-          <v-btn flat color="purple">Explore</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn icon @click="show = !show">
-            <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
-          </v-btn>
-        </v-card-actions>
-
-        <v-slide-y-transition>
-          <v-card-text v-show="show">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-          </v-card-text>
-        </v-slide-y-transition>
+        <v-list-tile>
+          <v-list-tile-content>
+            <v-list-tile-title v-html="pdata.title">
+              {{ pdata.title }}
+            </v-list-tile-title>
+            <v-list-tile-sub-title v-html="pdata.subtitle">
+              {{ pdata.subtitle }}
+            </v-list-tile-sub-title>
+          </v-list-tile-content>
+        </v-list-tile>
       </v-card>
     </v-flex>
   </v-layout>
 </template>
 <script>
-import '@mdi/font/css/materialdesignicons.css'
-import Vue from 'vue'
-import Vuetify from 'vuetify'
-Vue.use(Vuetify)
+import "@mdi/font/css/materialdesignicons.css";
+import Vue from "vue";
+import Vuetify from "vuetify";
+Vue.use(Vuetify);
 export default {
-name: 'Product',
-data: () => {
+  name: "Product",
+  data: () => {
     return {
-        show: false
-    }
-}
-}
+      show: false
+    };
+  },
+  props: {
+      pdata: {
+          type: [Object, String],
+          required: true,
+          default: () => {}
+      }
+  }
+};
 </script>
 
--- withoutTypescript/src/popup/components/Products.vue
+++ withoutTypescript/src/popup/components/Products.vue
@@ -1,40 +1,43 @@
 <template>
     <v-layout row wrap>
-        ProductData
+        <v-card height="352px" width="100%">
+            <v-list>
+                <Product v-for="(item, index) in productsData" :key="index" pdata="item" />
+            </v-list>
+        </v-card>
     </v-layout>
 </template>
 <script>
 import Product from "./Product";
-import '@mdi/font/css/materialdesignicons.css'
-import Vue from 'vue'
-import Vuetify from 'vuetify'
+import "@mdi/font/css/materialdesignicons.css";
+import Vue from "vue";
+import Vuetify from "vuetify";
 export default {
-name: 'Products',
-data: () => {
+  name: "Products",
+  data: () => {
     return {
-        productsData: [
+      productsData: [
         {
-            'title': 'iPhone XS',
-            'subtitle': 'Pretty af',
-            'imgSrc': '../../../static/images/iphone-xs.jpg'
+          title: "iPhone XS",
+          subtitle: "Pretty af",
+          imgSrc: "../../../static/images/iphone-xs.jpg"
         },
         {
-            'title': 'Galaxy Note 9',
-            'subtitle': 'Pretty af',
-            'imgSrc': '../../../static/images/galaxy-note-9.jpg'
+          title: "Galaxy Note 9",
+          subtitle: "Pretty af",
+          imgSrc: "../../../static/images/galaxy-note-9.jpg"
         },
         {
-            'title': 'Pixel 3',
-            'subtitle': 'Pretty af',
-            'imgSrc': '../../../static/images/pixel_3.jpg'
+          title: "Pixel 3",
+          subtitle: "Pretty af",
+          imgSrc: "../../../static/images/pixel_3.jpg"
         }
-    ]
-    }
-},
-components: {
+      ]
+    };
+  },
+  components: {
     Product
-},
-
-}
+  }
+};
 </script>
 
