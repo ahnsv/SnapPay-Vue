41a4269d2e4e15353e4a55daafa848a26836ff63
ahnsv <ahnsv@bc.edu>
2018-10-25 10:42:24 +0900

    store added

---
+++ withoutTypescript/src/popup/store/modules/checkout.js
@@ -0,0 +1,18 @@
+const state = {
+  isAutheticated: false | true,
+  authenticationData: null
+}
+
+const getters = {}
+
+const actions = {}
+
+const mutations = {}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+}
