cfe54bc7afac778ec199e496141c4c9d1e2f720b
Humphrey Ahn <ahnsv@bc.edu>
2018-10-26 23:58:54 +0900

    chrome side init

--- withoutTypescript/src/content/index.js
+++ withoutTypescript/src/content/index.js
@@ -1,1 +1,44 @@
-console.log('content-script!')
+import axios from 'axios'
+
+const getCurrentUrl = () => {
+    return chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, function (tabs) {
+        let url = tabs[0].url;
+        return url
+    });
+}
+
+// check if the current url is in the registered urls
+chrome.devtools.network.onNavigated.addListener(createPanelIfRegistered)
+const panelState = {
+    isCreated: false,
+    isLoaded: false,
+    isShown: false
+}
+const createPanelIfRegistered = (panelState) => {
+    if (panelState.isCreated || panelState.isLoaded || panelState.isShown ) return;
+    // check if url is registered or not
+    chrome.devtools.inspectedWindow.eval('', () => {
+        chrome.devtools.panels.create('SnapPay', 'icon', 'html', panel => {
+            chrome.runtime.sendMessage('SnapPay panel initiated!')
+        })
+    })
+    // set icons and popup
+    chrome.runtime.onMessage.addListener((req, sender) => {
+        // check tabid and stuffs
+        chrome.browserAction.setIcon({})
+        chrome.browserAction.setPopup({})
+    })
+}
+
+// highlights link or image
+const highlightElem = (hookElement) => {
+    // overlay => hookElement 
+    // https://github.com/vuejs/vue-devtools/blob/dev/src/backend/highlighter.js
+
+}
+
+const overlay = {
+
+}
+
+// push notification
--- withoutTypescript/src/popup/api/index.js
+++ withoutTypescript/src/popup/api/index.js
@@ -1,5 +1,4 @@
 import axios from 'axios'
-import { dev } from './config'
 export default () => {
   return axios.create({
     baseURL: 'http://localhost:3000/',
